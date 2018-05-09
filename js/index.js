256e40a5fe137e07a0e3a1124ce8648f81a71681
manglsh04 <manglesh.yadav04@gmail.com>
2018-05-09 19:15:15 +0530

    bs4 shell

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// $("#toggle-offcanvas").on('click', function () {
+//     $("main").toggleClass("offcanvas");
+// });
+
+$("#toggle-wizard-sidenav").on('click', function () {
+    $("#rootwizard").toggleClass("wizard-toggle");
+});
+
+$("#toggle-offcanvas").on('click', function () {
+    $("body").toggleClass("offcanvas");
+});
